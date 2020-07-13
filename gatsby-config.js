a20fedc13d6b3aca38a83830f3a0e3f6eb952f56
inhaq <p126013@nu.edu.pk>
2020-07-13 17:26:17 +0500

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Codivox`,
+    description: ``,
+    author: `@codivox`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#2496ED`,
+        theme_color: `#2496ED`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import {graphql, PageProps} from "gatsby";
+
+type IndexPageProps = {
+    site: {
+      siteMetadata: {
+        title: string
+      }
+    }
+}
+
+const IndexPage: React.FC<PageProps<IndexPageProps>> = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello {data.site.siteMetadata.title}!</h1>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const query = graphql`
+    query IndexQuery {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`
