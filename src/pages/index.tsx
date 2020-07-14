063b45861d7213c9263ef6d57d9f670f0fe46e1b
inhaq <p126013@nu.edu.pk>
2020-07-14 17:02:36 +0500

    prettier on files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import * as React from "react"
+import * as React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import {graphql, PageProps} from "gatsby";
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { graphql, PageProps } from 'gatsby';
 
 type IndexPageProps = {
-    site: {
-      siteMetadata: {
-        title: string
-      }
-    }
-}
+  site: {
+    siteMetadata: {
+      title: string;
+    };
+  };
+};
 
 const IndexPage: React.FC<PageProps<IndexPageProps>> = ({ data }) => {
   return (
@@ -18,17 +18,17 @@ const IndexPage: React.FC<PageProps<IndexPageProps>> = ({ data }) => {
       <SEO title="Home" />
       <h1>Hello {data.site.siteMetadata.title}!</h1>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
-    query IndexQuery {
-        site {
-            siteMetadata {
-                title
-            }
-        }
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
     }
-`
+  }
+`;
