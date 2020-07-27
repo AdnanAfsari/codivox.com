07ef47cdbb427135cd17ee2281ce6389a7ac01be
inhaq <p126013@nu.edu.pk>
2020-07-27 23:27:14 +0500

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#6670FD`,
         theme_color: `#6670FD`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/codivox.png`, // This path is relative to the root of the site.
       },
     },
     {
