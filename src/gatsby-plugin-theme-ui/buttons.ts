5bedb244a1cd41615e5b4e72eff8cdc592753a77
inhaq <p126013@nu.edu.pk>
2020-07-29 13:48:03 +0500

    theme ui refactor

--- src/gatsby-plugin-theme-ui/buttons.ts
+++ src/gatsby-plugin-theme-ui/buttons.ts
@@ -1,21 +1,4 @@
-// @ts-ignore
-import { tailwind } from '@theme-ui/presets';
-
 export default {
-  ...tailwind,
-  colors: {
-    ...tailwind.colors,
-    main: '#6670FD',
-    background: 'white',
-  },
-  fonts: {
-    body: 'Lato, sans-serif',
-    heading: 'Josefin Sans, sans-serif',
-  },
-  fontWeight: {
-    body: 400,
-    heading: 700,
-  },
   buttons: {
     outlined: {
       width: 130,
