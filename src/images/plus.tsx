663d5b8f1a54be6783295a0af4459c540ff78d2c
Adnan Afsari <aarahul1991@gmail.com>
2020-08-05 17:45:38 +0600

    Add TechStack iCons

---
+++ src/images/plus.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function PlusSvg(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={28} height={27} viewBox="0 0 28 27" fill="none" {...props}>
+      <path
+        d="M26.6 12.15H15.4V1.35C15.4.605 14.773 0 14 0c-.773 0-1.4.605-1.4 1.35v10.8H1.4c-.773 0-1.4.605-1.4 1.35 0 .745.627 1.35 1.4 1.35h11.2v10.8c0 .745.627 1.35 1.4 1.35.773 0 1.4-.605 1.4-1.35v-10.8h11.2c.773 0 1.4-.605 1.4-1.35 0-.745-.627-1.35-1.4-1.35z"
+        fill="#6670FD"
+        fillOpacity={0.75}
+      />
+    </svg>
+  );
+}
+
+export default PlusSvg;
---
+++ src/images/sendgrid.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+function SendgridSvg(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={40} height={39} viewBox="0 0 40 39" fill="none" {...props}>
+      <g clipPath="url(#prefix__clip0)">
+        <path d="M13.333 13H0v26h26.667V13H13.333z" fill="#9DE1F3" />
+        <path d="M13.334 0v26H40V0H13.334z" fill="#27B4E1" />
+        <path
+          d="M0 39h13.333V26H0v13zm26.667-26H40V0H26.667v13z"
+          fill="#1A82E2"
+        />
+        <path d="M13.334 26h13.333V13H13.333v13z" fill="#239FD7" />
+      </g>
+      <defs>
+        <clipPath id="prefix__clip0">
+          <path fill="#fff" d="M0 0h40v39H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default SendgridSvg;
