08b6e9d55db7435a8117d980d5539818c8197bd7
Adnan Afsari <aarahul1991@gmail.com>
2020-08-10 14:28:25 +0600

    fix Footer FB social icon

--- src/images/fb.tsx
+++ src/images/fb.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 
-function Fb(props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) {
+function Fb(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg width={18} height={18} viewBox="0 0 18 18" fill="none" {...props}>
-      <g clipPath="url(#prefix__clip0)">
-        <path
-          d="M6.744 17.983v-7.845H4.343V6.61h2.456V3.69S6.911.011 10.062-.018h3.507v3.463h-2.175s-.919 0-.919 1.035v2.191h3.147l-.378 3.527H10.53v7.785H6.744z"
-          fill="#141633"
-        />
-      </g>
-      <defs>
-        <clipPath id="prefix__clip0">
-          <path fill="#fff" d="M0 0h18v18H0z" />
-        </clipPath>
-      </defs>
+    <svg width={18} height={18} viewBox="0 0 14 25" fill="none" {...props}>
+      <path
+        d="M3.978 24.976V14.08H.643V9.18h3.412V5.124S4.21.015 8.586-.024h4.871v4.81h-3.021s-1.276 0-1.276 1.437v3.042h4.37l-.525 4.898H9.236v10.813H3.978z"
+        fill="#141633"
+      />
     </svg>
   );
 }
