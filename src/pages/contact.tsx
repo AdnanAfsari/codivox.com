6102f4395ed39cc5bf4578e76b08ce0064d12264
Adnan Afsari <aarahul1991@gmail.com>
2020-08-14 19:09:37 +0600

    Create contact page structure

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { graphql, PageProps } from 'gatsby';
+
+import Layout from '../components/layout';
+import Hi from '../components/Main/Hi';
+
+const ContactPage: React.FC = () => {
+  return <Layout></Layout>;
+};
+
+export default ContactPage;
