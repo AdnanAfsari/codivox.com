3660acc61421c2733334dc84e6c1a9f3eb5d9e7d
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-08-14 20:01:29 +0600

    Setup contact page for adopt modal layout

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,38 @@
+/** @jsx jsx */
+import { Box, Heading, jsx } from 'theme-ui';
 import React from 'react';
-import { graphql, PageProps } from 'gatsby';
+import { useStaticQuery, graphql, PageProps } from 'gatsby';
+// @ts-ignore
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-import Layout from '../components/layout';
-import Hi from '../components/Main/Hi';
+import Header from '../components/header';
+import Footer from '../components/Main/Footer';
 
 const ContactPage: React.FC = () => {
-  return <Layout></Layout>;
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuerySecond {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <React.Fragment>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div sx={{ backgroundColor: 'red', height: '100px', width: '100%' }}>
+        fff
+      </div>
+      <Footer />
+      <MessengerCustomerChat
+        pageId={process.env.GATSBY_PAGE_ID}
+        appId={process.env.GATSBY_APP_ID}
+        themeColor="#6670FD"
+      />
+    </React.Fragment>
+  );
 };
 
 export default ContactPage;
