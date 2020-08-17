83e99b34d130965ad74628b617c485c6a473e792
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-08-17 13:15:54 +0600

    Add union and warning icons + contact page modal trigger complete

--- src/components/Contact/Modal.tsx
+++ src/components/Contact/Modal.tsx
@@ -1,18 +1,25 @@
 /** @jsx jsx */
-import { Box, Heading, jsx } from 'theme-ui';
+import { Box, Flex, Heading, jsx } from 'theme-ui';
 import React from 'react';
 import { useStaticQuery, graphql, PageProps } from 'gatsby';
+import Warning from '../../images/warning';
+import Union from '../../images/union';
+import flex from '../../gatsby-plugin-theme-ui/flex';
 
 const Modal: React.FC = () => {
   return (
     <React.Fragment>
       <div
-        sx={{ backgroundColor: '#6670FD', height: '50px', lineHeight: '100%' }}
+        sx={{
+          backgroundColor: '#6670FD',
+          height: '50px',
+          alignItems: 'center',
+          display: 'flex',
+        }}
       >
         <div
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
+            margin: '0 auto',
           }}
         >
           <Box
@@ -24,14 +31,47 @@ const Modal: React.FC = () => {
               px: 3,
             }}
           >
-            <p>Hey there, before you decide to contact us, see if we match.</p>
-            <p
+            <Flex
               sx={{
-                justifySelf: 'end',
+                display: 'flex',
+                alignItems: 'center',
+                color: '#fff',
+                fontWeight: 'bold',
+                fontSize: '16px',
               }}
             >
-              Hey there, before you decide to contact us, see if we match.
-            </p>
+              <Warning />
+              <p
+                sx={{
+                  paddingLeft: '10px',
+                }}
+              >
+                Hey there, before you decide to contact us, see if we match.
+              </p>
+            </Flex>
+
+            <Flex
+              sx={{
+                justifySelf: 'end',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <button
+                sx={{
+                  pr: '5px',
+                  background: 'none',
+                  border: 'none',
+                  color: '#fff',
+                  fontWeight: 'bold',
+                  textDecoration: 'underline',
+                  fontSize: '16px',
+                }}
+              >
+                See if we match
+              </button>
+              <Union />
+            </Flex>
           </Box>
         </div>
       </div>
