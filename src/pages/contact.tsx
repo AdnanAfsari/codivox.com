088a4cbe9b3b295cafff0d128aa0396b1aab417e
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-08-19 14:20:26 +0600

    Fix:: Modal component naming

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { Box, Heading, jsx } from 'theme-ui';
 import React from 'react';
-import ModalCom from '../components/Contact/Modal';
+import ModalCom from '../components/Contact/modal';
 import ContactForm from '../components/Contact/contactForm';
 
 const ContactPage: React.FC = () => {
