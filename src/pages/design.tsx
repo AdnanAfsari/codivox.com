2d0258c3dccd8db4fc6531a38a373387c5c66fc7
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-08-24 14:20:14 +0600

    Add Hi component in Design page

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hi from '../components/Main/Hi';
 
 const DesignPage: React.FC = () => {
   return (
     <React.Fragment>
       <h1>Hi there I am DesignPage</h1>
+      <Hi />
     </React.Fragment>
   );
 };
