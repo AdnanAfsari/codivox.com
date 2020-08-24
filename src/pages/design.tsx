17ff455ed5c8bb8337c013fdb7f0ad66ea6a0edd
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-08-24 14:17:19 +0600

    Create Desing page

---
+++ src/pages/design.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DesignPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>Hi there I am DesignPage</h1>
+    </React.Fragment>
+  );
+};
+
+export default DesignPage;
