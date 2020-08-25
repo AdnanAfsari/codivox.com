9031f1a8d3023d4f579f51acc56c0659e01eb783
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-08-25 14:14:15 +0600

    Design page's Webdesing componnet's complete with responsiveness

--- src/components/Design/webDesign.tsx
+++ src/components/Design/webDesign.tsx
@@ -1,10 +1,11 @@
 /** @jsx jsx */
 import { jsx, Box, Flex, Heading } from 'theme-ui';
 import React from 'react';
-import DesingBrandImg from '../../images/designBrandImg';
+import WebDesignImg from '../../images/webDesignImg';
 
 const WebDesign: React.FC = () => {
-  const brandTitle = 'From logo and branding to print and social media designs';
+  const brandTitle =
+    'We put smart efforts to research and planning to execute amazing experience';
   const brandSubtitle =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In imperdiet dui non ex maximus varius. Fusce interdum felis at aliquet maximus. Vivamus tempor mauris ut molestie cursusm a.';
   return (
@@ -19,43 +20,48 @@ const WebDesign: React.FC = () => {
       <Box sx={{ textAlign: 'center', pt: '77px' }}>
         <Heading
           sx={{
-            fontSize: ['26px', '38px'],
-            width: ['100%', 480],
+            fontSize: ['26px', '36px'],
+            width: ['100%', '620px'],
             m: '0 auto',
             fontWeight: 600,
           }}
         >
-          Make your brands go into the future
+          Web design services that help you to boost more revenue
         </Heading>
-        <p sx={{ opacity: '0.85' }}>
-          We offers a rise-above attitude, a holistic approach to branding
+        <p sx={{ opacity: '0.85', width: ['100%', '446px'], m: '0 auto' }}>
+          We deliver quality web apps design based on measurable results to get
+          higher conversions.
         </p>
       </Box>
 
       <Box
         sx={{
           display: 'grid',
-          gridTemplateColumns: ['100%', '50% 55%'],
+          gridTemplateColumns: ['100%', '45% 55%'],
+          gridTemplateRows: '1fr',
           gap: '5%',
-          gridAutoFlow: ['dense', null],
           alignItems: 'center',
           mt: '25px',
         }}
       >
+        <WebDesignImg
+          sx={{
+            width: '100%',
+          }}
+        />
         <Flex
           sx={{
             display: 'grid',
             gridTemplateRows: 'auto auto auto',
             height: ['calc( 300px )', 'calc( 400px )'],
             gridTemplateColumns: ['100%'],
-            gridColumn: [1, null],
             textAlign: ['center', 'left'],
           }}
         >
           <Heading
             sx={{
               fontSize: ['27px', '28px'],
-              maxWidth: '450px',
+              maxWidth: '455px',
               mt: ['10px', '100px'],
             }}
           >
@@ -63,13 +69,6 @@ const WebDesign: React.FC = () => {
           </Heading>
           <p sx={{ opacity: '0.85', mt: ['10px', '-35px'] }}>{brandSubtitle}</p>
         </Flex>
-        <DesingBrandImg
-          sx={{
-            gridRow: [1, 'unset'],
-            gridColumn: [1, 'unset'],
-            width: '100%',
-          }}
-        />
       </Box>
     </Box>
   );
