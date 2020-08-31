3a4f993de5ec705af205557333b923d36bbf2573
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-09-01 02:44:46 +0600

    Web Developement page's hero title change

--- src/pages/webdevelopment.tsx
+++ src/pages/webdevelopment.tsx
@@ -2,7 +2,7 @@
 import { jsx } from 'theme-ui';
 import React from 'react';
 import Hi from '../components/Main/Hi';
-import Hero from '../components/Design/Hero';
+import Hero from '../components/WebDevelopment/Hero';
 import Brand from '../components/Design/Brand';
 import WebDesign from '../components/Design/webDesign';
 import AppDesign from '../components/Design/appDesign';
