a271513c1702e5578c362398de19913c2c5cf146
shjang7 <suhy.jang.shj@gmail.com>
2020-09-10 13:31:07 +0900

    Read single post

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,3 +25,38 @@ exports.onCreatePage = ({ page, actions }) => {
     createPage(page);
   }
 };
+
+const path = require(`path`);
+const { slash } = require(`gatsby-core-utils`);
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // Will create pages for WordPress posts (route : /blog/{slug})
+  const res = await graphql(`
+    query {
+      wpgraphql {
+        posts {
+          nodes {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (res.errors) {
+    console.error(res.errors);
+  }
+
+  const { wpgraphql } = res.data;
+  const blogTemplate = path.resolve('./src/templates/blog-template.tsx');
+  wpgraphql.posts.nodes.forEach(({ id, slug }) => {
+    createPage({
+      path: `/blog/${slug}/`,
+      component: slash(blogTemplate),
+      context: { id },
+    });
+  });
+};
--- src/templates/blog-template.tsx
+++ src/templates/blog-template.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, Heading, Flex } from 'theme-ui';
+import { Fragment } from 'react';
+import { graphql } from 'gatsby';
+import PostSummary from '../components/Blog/PostSummary';
+import { SinglePostProps } from '../types/blog';
 
-const BlogTemplate = () => {
-  return <div>single blog post</div>;
+interface BlogTemplateProps {
+  data: SinglePostProps;
+}
+
+const BlogTemplate = ({ data }: BlogTemplateProps) => {
+  const { post } = data.wpgraphql;
+  const {
+    slug,
+    title,
+    date,
+    author,
+    categories,
+    featuredImage,
+    excerpt,
+    content,
+    tags,
+  } = post;
+  return (
+    <Fragment>
+      <PostSummary
+        post={{ slug, title, date, author, categories, featuredImage }}
+      />
+      <div>
+        <Heading dangerouslySetInnerHTML={{ __html: excerpt }} />
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </Fragment>
+  );
 };
 
 export default BlogTemplate;
+
+export const singlePostQuery = graphql`
+  query($id: ID!) {
+    wpgraphql {
+      post(id: $id) {
+        slug
+        title(format: RENDERED)
+        date
+        author {
+          node {
+            name
+          }
+        }
+        categories {
+          nodes {
+            slug
+            name
+          }
+        }
+        featuredImage {
+          node {
+            altText
+            title
+            sourceUrl
+          }
+        }
+        excerpt(format: RENDERED)
+        content(format: RENDERED)
+        tags {
+          nodes {
+            slug
+            name
+          }
+        }
+      }
+    }
+  }
+`;
