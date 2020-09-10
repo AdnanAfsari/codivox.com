9194705ef62542c4a1fcd70de3965996204b4a04
shjang7 <suhy.jang.shj@gmail.com>
2020-09-10 15:29:52 +0900

    Update settings for seperate layouts

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,26 +5,6 @@
  */
 
 // You can delete this file if you're not using it
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions;
-
-  if (page.path.match(/contact/)) {
-    page.context.layout = 'contact';
-    createPage(page);
-  }
-  if (page.path.match(/design/)) {
-    page.context.layout = 'design';
-    createPage(page);
-  }
-  if (page.path.match(/appdevelopment/)) {
-    page.context.layout = 'appdevelopment';
-    createPage(page);
-  }
-  if (page.path.match(/webdevelopment/)) {
-    page.context.layout = 'webdevelopment';
-    createPage(page);
-  }
-};
 
 const path = require(`path`);
 const { slash } = require(`gatsby-core-utils`);
